946212acecba3fd75a063121cb6144d683bbf918
Charbel Fannoun <charbelrnd@gmail.com>
2019-05-18 19:36:51 +0300

    Adding missing files

---
+++ src/main/java/com/retail/retailappservices/RetailAppServicesApplication.java
@@ -0,0 +1,13 @@
+package com.retail.retailappservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailAppServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailAppServicesApplication.class, args);
+	}
+
+}
